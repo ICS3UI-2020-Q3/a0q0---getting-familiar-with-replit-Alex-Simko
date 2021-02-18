06ebc46d83316646a7b8cfc610813332c4a03333
AlexSimko <simka1861@wrdsb.ca>
2021-02-18 14:48:41 +0000

    Added Header and completed Print Hello World!

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * Hello World Program
+ * @author Alex-Simko 
  */
 public class Main {
 
@@ -8,8 +8,10 @@ public class Main {
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
+
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    System.out.println("Hello World!");
   }
+
 }
